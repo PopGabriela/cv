a542b9f8500c2c6da2906c4483e620a3200580a4
PopGabriela <116027616+PopGabriela@users.noreply.github.com>
2022-10-17 21:15:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function afiseazaText(){
+    document.querySelector("#experienta1").classList.toggle("element-ascuns");
+    //document.querySelector -->ne returneaza elementul
+    //classlist --> ne ofera o lista cu clasele
+    //togle--> va adauga o clasa in cazul in care ea nu este in lista , si o va sterge daca este in lista (se aplica doar pt classlist)
+}
+ function afiseazaText2 (){
+    document.querySelector("#experienta2").classList.toggle("element-ascuns");
+ }
